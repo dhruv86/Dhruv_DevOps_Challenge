84fbf40de3ec523b8d45f69314df5270b2f5171b
dhruv86 <dhruv86happy@gmail.com>
2025-06-26 20:01:15 +0000

    Update the tf backend with the correct storage account name

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "terraform-state-rg"
-    storage_account_name = "terraformstateaccount"
+    storage_account_name = "terraformstateaccount186"
     container_name       = "terraform-state"
     key                  = "terraform.tfstate"
   }
