ecc908a617fdf1d32577cfb45924827cb9f3acb2
dhruv86 <dhruv86happy@gmail.com>
2025-06-26 19:49:25 +0000

    Initial setup for terraform workflow and backend

---
+++ terraform/backend.tf
@@ -0,0 +1,21 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "terraform-state-rg"
+    storage_account_name = "terraformstateaccount"
+    container_name       = "terraform-state"
+    key                  = "terraform.tfstate"
+  }
+  
+  required_version = ">= 1.0"
+  
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
